2edc11433a36c4bf272fb0f503d5d2d932432657
HannahConnolly <connollyhannah93@gmail.com>
2023-10-05 03:07:14 -0400

    added loop

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from mta_api import MtaApi
 from print_dash import PrintDash
 import json
 from tabulate import tabulate
+import time
 
 use_local = False
 
@@ -12,10 +13,12 @@ class PulaskiDash:
     def __init__(self):
         train_getter = MtaApi()
         print_dash = PrintDash()
-        train_board = train_getter.get_trains(use_local)
-        print(train_board)
-        print_dash.format_trains_to_table(train_board)
-        
+        while(True):
+            train_board = train_getter.get_trains(use_local)
+            print(train_board)
+            print_dash.format_trains_to_table(train_board)
+            time.sleep(60)
+          
     
 
 
