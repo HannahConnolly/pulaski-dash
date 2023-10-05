d58c0002af191ddcfddff5d188ea74a07d73e5d3
HannahConnolly <connollyhannah93@gmail.com>
2023-10-05 17:28:24 -0400

    light colors

--- app.py
+++ app.py
@@ -3,8 +3,16 @@ from print_dash import PrintDash
 import json
 from tabulate import tabulate
 import time
-use_local = False
 
+from colorama import Fore, Back, Style
+
+print(Fore.RED + 'some red text')
+print(Back.GREEN + 'and with a green background')
+print(Style.DIM + 'and in dim text')
+print(Style.RESET_ALL)
+print('back to normal now')
+
+use_local = False
 
 train_getter = MtaApi()
 print_dash = PrintDash()
@@ -12,8 +20,8 @@ while(True):
   train_board = train_getter.get_trains(use_local)
   x = 0
   while x < 10:
-    print('\n')
+    print(Back.WHITE + '\n')
     x+=1
-  print(tabulate(print_dash.format_trains_to_table(train_board)))
+  print(Fore.BLACK + Back.WHITE + tabulate(print_dash.format_trains_to_table(train_board)))
   time.sleep(60)
 
